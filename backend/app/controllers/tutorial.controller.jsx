04fd2aa36107bab3ff2df47cd1fd9d4729826dd3
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-02-01 01:25:25 +0530

    Add files via upload

--- backend/app/controllers/tutorial.controller.jsx
+++ backend/app/controllers/tutorial.controller.jsx
@@ -1,1 +1,129 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+exports.create = (req,res) => {
+    if(!req.body.title){
+        res.status(400).send({message: "Content can not be empty!"});
+        return;
+    }
+
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published? req.body.published : false
+    });
+
+    tutorial.save(tutorial).then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "some error occured while creating the Tutorial."
+        });
+    });
+};
+
+exports.findAll = (req,res) =>{
+    const title = req.query.title;
+    var condition = title ? {title: {$regex: new RegExp(title), $options: "i"}}: {};
+
+    Tutorial.find(condition)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Some error occured"
+        });
+    });
+};
+
+exports.findOne = (req,res) => {
+    const id = req.params.id;
+
+    Tutorial.findById(id)
+    .then(data => {
+        if (!data)
+        res.status(404).send({message: "Not found" + id});
+        else res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({message: "Error retrieving:" + id});
+    });
+};
+
+exports.update = (req,res) => {
+    if(!req.body) {
+        return res.status(400).send({
+            message: "Cannot be empty"
+        });
+    }
+
+    const id = req.params.id;
+
+    Tutorial.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+    .then(data => {
+        if(!data) {
+            res.status(404).send({
+                message: `Cannot update with id=${id}. May Tutorial not found!`
+            });
+        } else res.send({message: "Tutorial updated!"});
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Error updating"
+        })
+    })
+};
+
+exports.delete = (req,res) => {
+    const id = req.params.id;
+    Tutorial.findByIdAndRemove(id)
+    .then(data => {
+        if(!data) {
+            res.status(404).send({
+                message: `Cannot delete, cause item not found`
+            });
+        } else {
+            res.send({
+                message: "tutorial Deleted "
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete" + id
+        });
+    });
+
+};
+
+exports.deleteAll = (req,res) => {
+    Tutorial.deleteMany({})
+    .then(data => {
+        res.send({
+            message: `{data.deletedCount} Tutorials were deleted.`
+        });
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message: err.message || "some error occured"
+        });
+    });
+};
+
+exports.findAllPublished = (req,res) => {
+    Tutorial.find({published: true})
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "some error occured"
+        });
+    });
+};
+
+
 
