ac8911ed74705e06708b90ba3ba74b38cd83eaa4
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-02-01 01:28:43 +0530

    Add files via upload

--- backend/app/routes/tutorial.routes.jsx
+++ backend/app/routes/tutorial.routes.jsx
@@ -1,1 +1,21 @@
+module.exports = app => {
+    const tutorials = require("C:/Nodejs/nodejs-express-mongodb/app/controllers/tutorial.controller.jsx");
+    var router = require("express").Router();
 
+    router.post("/", tutorials.create);
+
+    router.get("/", tutorials.findAll);
+
+    router.get("/published", tutorials.findAllPublished);
+   
+    router.get("/:id", tutorials.findOne);
+
+    router.put("/:id", tutorials.update);
+
+    router.delete("/:id", tutorials.delete);
+
+    router.delete("/", tutorials.deleteAll);
+
+    app.use('/api/tutorials', router);
+    
+}
